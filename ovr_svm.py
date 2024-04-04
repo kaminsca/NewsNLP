5c5b703c46e3343461f698b2021f8f8fd8e7ddd1
Jakob Louis Therkelsen <therkels@msu.edu>
2024-04-04 18:09:06 -0400

    output metrics

--- ovr_svm.py
+++ ovr_svm.py
@@ -1,6 +1,8 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.datasets import make_classification
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,recall_score,f1_score
 from sklearn.svm import SVC
 import pandas as pd
 from joblib import dump
@@ -10,18 +12,27 @@ if __name__ == "__main__":
     df = pd.read_csv('./output/processed_data.csv', delimiter='|')
 
     corpus = df['content'].fillna('').to_list()
-    print(corpus[:10])
-    vectorizer = TfidfVectorizer()
+
+
+
+    vectorizer = TfidfVectorizer(stop_words="english")
     X = vectorizer.fit_transform(corpus)
 
-    print(vectorizer.get_feature_names_out()) 
     Y = df.iloc[:, 5:].values
-    print("done")
 
-    clf = OneVsRestClassifier(SVC(kernel='sigmoid', verbose=True)).fit(X,Y)
+    x_train,x_test,y_train,y_test = train_test_split(X, df.iloc[:, 5:].values, test_size=0.3)
+
+
+    clf = OneVsRestClassifier(SVC(kernel='rbf', verbose=True)).fit(x_train,y_train)
     dump(vectorizer, 'tfidf.joblib')
     dump(clf, 'ovr-svm.joblib')
-    # test_text = ["Trump goes to Zoo"]
-    # test_vector = vectorizer.transform(test_text)
-    # prediction = clf.predict(test_vector)
-    # print(f"prediction: {prediction}")
+
+
+    y_pred = clf.predict(x_test)
+    accuracy = accuracy_score(y_test,y_pred)
+    recall = recall_score(y_test,y_pred, average="macro")
+    f1_score = f1_score(y_test,y_pred, average="macro")
+    print("")
+    print(f"Accuracy: {accuracy}")
+    print(f"Recall: {recall}")
+    print(f"f1: {f1_score}")
